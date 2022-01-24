f12c5f53cb317dd60dad79d065afe323f4b1a6a2
stevenbooke <43659151+stevenbooke@users.noreply.github.com>
2022-01-24 14:27:48 -0800

    first commit

---
+++ src/main/java/com/springbootbasketball/springbootbasketball/SpringBootBasketballApplication.java
@@ -0,0 +1,13 @@
+package com.springbootbasketball.springbootbasketball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasketballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasketballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootbasketball/springbootbasketball/SpringBootBasketballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootbasketball.springbootbasketball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBasketballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
